59a2ffd9701e2e6742b5bedc15ed509285baeb19
BranSanmamede <bran.bouzas.prado@iessanmamede.com>
2025-02-04 13:10:04 +0000

    top tracks casi terminado

--- src/app/components/topTracks/topTracks.ts
+++ src/app/components/topTracks/topTracks.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UtilService } from '../../services/utilService';
 import { Album } from '../../interfaces/album';
+import { Song } from '../../interfaces/song';
 
 @Component({
   selector: 'app-topTracks',
@@ -10,8 +11,10 @@ import { Album } from '../../interfaces/album';
 })
 export class TopTracksComponent implements OnInit{
   
+  songs: Song[] = [];
   albums: Album[] = [];
   selectedAlbum: Album | null = null;
+  showButton: boolean = false;
   
   constructor(private utilService: UtilService, private router: Router) {}
   
@@ -19,11 +22,23 @@ export class TopTracksComponent implements OnInit{
     this.albums = this.utilService.getAlbums();
     if(this.albums.length > 0) {
       this.selectedAlbum = this.albums[0];
+      this.loadSongsForAlbum();
+    }
+  }
+
+  loadSongsForAlbum() {
+    if(this.selectedAlbum != null) {
+      if(this.selectedAlbum.songs.length) {
+        this.songs = this.selectedAlbum.songs.map(id => this.utilService.getSongByID(id)).filter(song => song !== undefined);
+      }
+    }else {
+      this.songs = [];
     }
   }
 
   selectAlbum(album: Album) {
     this.selectedAlbum = album;  
+    this.loadSongsForAlbum();
   }
   
   showAlbum(id: number) {
